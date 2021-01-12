a005b15a8cef40b6c6707ac74f63cefe512093a1
Ross MacDonald <42966821+RossmacD@users.noreply.github.com>
2021-01-12 17:30:43 +0000

    Added Navbar

--- src/App/App.jsx
+++ src/App/App.jsx
@@ -1,10 +1,13 @@
 import './App.css';
 import { Component } from 'react';
+import Navbar from '../components/navbar'
+
 
 class App extends Component {
   render() {
     return (
       <div className="App">
+        <Navbar></Navbar>
         <p>This is my app</p>
       </div>)
   }
--- src/components/navbar.jsx
+++ src/components/navbar.jsx
@@ -1,9 +1,26 @@
-import Component from 'react'
+import { Component } from 'react'
+
+import { Menu } from '@fluentui/react-northstar'
 
 class Navbar extends Component {
     render() {
         return (
-            <div></div>
+            <Menu defaultActiveIndex={0} >
+                <Menu.Item index={0}>
+                    <Menu.ItemContent>Home</Menu.ItemContent>
+                </Menu.Item>
+                <Menu.Item index={1}>
+                    <Menu.ItemContent>Login</Menu.ItemContent>
+                </Menu.Item>
+                <Menu.Item index={2}>
+                    <Menu.ItemContent>Register</Menu.ItemContent>
+                </Menu.Item>
+                <Menu.Item index={3}>
+                    <Menu.ItemContent>Logout</Menu.ItemContent>
+                </Menu.Item>
+            </Menu>
         )
     }
 }
+
+export default Navbar;
