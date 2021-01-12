fafd06cb65f5bd9b549300b2b1144c8fb2b958b7
Ross MacDonald <42966821+RossmacD@users.noreply.github.com>
2021-01-12 17:16:55 +0000

    Init with fabricui

---
+++ src/App/App.jsx
@@ -0,0 +1,13 @@
+import './App.css';
+import { Component } from 'react';
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <p>This is my app</p>
+      </div>)
+  }
+}
+
+export default App;
